75e669d2cf5999043185036cf5bb5fab35bdda56
baijian.ying <baijian.ying@tuya.com>
2023-03-06 14:06:35 +0800

    add mbedtls files

--- wallyingLib.h
+++ wallyingLib.h
@@ -3,9 +3,12 @@
 
 #include "stdint.h"
 
+
 void hexdump(const char *name, const void *buf, uint16_t len);
 
+
 int32_t rand_number(int32_t min, int32_t max);
+
 
 void    bit_set(uint8_t *data, uint32_t bit_idx, uint8_t bit_value);
 
@@ -14,6 +17,7 @@ uint8_t bit_get(const uint8_t *data, uint32_t bit_idx);
 uint32_t bits_count(const uint8_t *data, uint32_t bit_num);
 
 void bits_reverse(uint8_t *data, uint32_t bit_num);
+
 
 void array_reverse(uint8_t *array, uint16_t length);
 
